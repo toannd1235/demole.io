03b658ae9ea72630f45348aba178915791d46b1d
toannd <toannadi@gmail.com>
2022-04-21 14:35:11 +0700

    fix ui

--- assets/js/scoll.js
+++ assets/js/scoll.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
 
 $(window).scroll(function () {
   if ($(window).scrollTop() <= 90) {
-    $(".navigation a.active").removeClass("active");
+    $(".navigation li.active").removeClass("active");
   }
 });
 $(window).scroll(function () {
@@ -54,3 +54,54 @@ $(window).scroll(function () {
     $(".header-mobile").removeClass("header-bg");
   }
 });
+
+$(window).scroll(function () {
+  active();
+});
+function active() {
+  let height_1 = $("#intro").height();
+  let height_2 = $("#feature").height();
+  let height_3 = $("#token").height();
+  let height_4 = $("#roadmap").height();
+
+  let distance_1 = $("#intro").offset().top;
+  let distance_2 = $("#feature").offset().top;
+  let distance_3 = $("#token").offset().top;
+  let distance_4 = $("#roadmap").offset().top;
+  let scrollY = window.pageYOffset;
+  console.log(distance_1, height_1);
+  if (scrollY >= distance_1 - 100 && scrollY <= distance_1 + height_1) {
+    addTo(" .header-1");
+    return;
+  }
+  if (scrollY > distance_2 - 100 && scrollY <= distance_2 + height_2) {
+    addTo(" .header-2");
+    return;
+  }
+  if (scrollY > distance_3 - 100 && scrollY <= distance_3 + height_3) {
+    addTo(" .header-3");
+    return;
+  }
+
+  if (scrollY > distance_4 - 100) {
+    addTo(" .header-4");
+    return;
+  }
+}
+function addTo(name) {
+  const prefix = "header .navigation";
+  let to = $(prefix + " li.to").index();
+  let from = $(prefix + " li.from").index();
+  if (from === -1) {
+    $(prefix + " li").removeClass("active");
+    $(prefix + name).addClass("active");
+    $(prefix + " li").removeClass("to");
+    $(prefix + name).addClass("to");
+  } else {
+    if (Math.abs(to - from) > 0) {
+      if ($(prefix + name).hasClass("to")) {
+        $(prefix + " li").removeClass("from");
+      }
+    }
+  }
+}
